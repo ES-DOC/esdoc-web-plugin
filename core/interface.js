de2a4ff92cdb9ee7bc02256bb61206005c3f28d9
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-03-21 15:36:17 +0100

    version bump

--- core/interface.js
+++ core/interface.js
@@ -8,7 +8,7 @@
     // Initialise root object.
     root.CIM = root.cim = root.ESDOC = root.esdoc = {
         // App version.
-        VERSION: '0.9.7.0',
+        VERSION: '0.9.7.1',
 
         // App name.
         NAME: "Earth System Documentation",
